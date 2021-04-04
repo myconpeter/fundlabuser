f8a3cc74e3eb6c615a132be304873821e7aea9fa
mycon <michealpeter040@gmail.com>
2021-04-04 04:25:49 +0200

    idjffmmfmf

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const app     = express();
 const bodyParser = require('body-parser');
 const PORT    = 8000;
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 
+// app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
+
 
 app.set("view engine", "ejs");
 
 app.get('/', (req, res)=>{
-    res.render('loin')
+    res.render('index')
 })
+
+app.get('/tips', (req, res)=>{
+    res.render('tips')
+})
+
+
+app.get("/login", (req, res)=>{
+    res.render('login')
+})
+
+
+
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
+
+app.get('/vendors', (req, res)=>{
+    res.render('vendors')
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+
 
 
 
